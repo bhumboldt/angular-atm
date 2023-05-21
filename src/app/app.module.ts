b42c4ff502bf0eaa2e3a48f070fe14bb0dc7d312
Brandon Humboldt <brhumboldt@gmail.com>
2023-05-21 12:57:02 -0500

    ^ add ngrx effects package     - remove default app component implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { StoreModule } from '@ngrx/store';
 import { CoreFundsModule } from '../lib/core/funds/core-funds.module';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { EffectsModule } from '@ngrx/effects';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,9 @@ import { StoreDevtoolsModule } from '@ngrx/store-devtools';
     StoreDevtoolsModule,
     CoreFundsModule,
 
-    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() })
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() }),
+
+    EffectsModule.forRoot([])
   ],
   providers: [],
   bootstrap: [AppComponent]
