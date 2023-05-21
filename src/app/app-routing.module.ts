778996fbdfdbcba115116d3d9fd18fa53d9eb392
Brandon Humboldt <brhumboldt@gmail.com>
2023-05-21 10:51:44 -0500

    * install ngrx, angular material packages     - remove base app component styles     * initial css util classes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'public',
+    children: [
+      {
+        path: '',
+        redirectTo: 'withdraw',
+        pathMatch: 'full'
+      },
+      {
+        path: 'withdraw',
+        loadChildren: () => import('../lib/pages/public/withdraw/withdraw.module').then(m => m.WithdrawModule)
+      },
+    ]
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'public'
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StoreModule } from '@ngrx/store';
+import { CoreFundsModule } from '../lib/core/funds/core-funds.module';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    StoreModule.forRoot({}, {}),
+    StoreDevtoolsModule,
+    CoreFundsModule,
+
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() })
   ],
   providers: [],
   bootstrap: [AppComponent]
